bfa49df31f67d7cd0c35248b4bed5285f0061f31
kuangben <kuangben2010@yeah.net>
2015-02-24 14:54:49 +0800

    aaa

--- AIEditor/gen-qt-projectfile.bat
+++ AIEditor/gen-qt-projectfile.bat
@@ -8,18 +8,18 @@ if exist %OUT_PRO% del %OUT_PRO%
 echo. > %OUT_PRO%
 
 echo TRANSLATIONS = \ >> %OUT_PRO%
-for /r %CUR_DIR%\QtBTEditor\languages %%a in (*.ts) do echo     %%a \ >> %OUT_PRO%
+for /r %CUR_DIR%\QtAIEditor\languages %%a in (*.ts) do echo     %%a \ >> %OUT_PRO%
 echo. >> %OUT_PRO%
 
 
 echo FORMS = \ >> %OUT_PRO%
-for /r %CUR_DIR%\QtBTEditor %%a in (*.ui) do echo     %%a \ >> %OUT_PRO%
+for /r %CUR_DIR%\QtAIEditor %%a in (*.ui) do echo     %%a \ >> %OUT_PRO%
 echo. >> %OUT_PRO%
 
 echo SOURCES = \ >> %OUT_PRO%
-for /r %CUR_DIR%\QtBTEditor\Src %%a in (*.cpp) do echo     %%a \ >> %OUT_PRO%
+for /r %CUR_DIR%\QtAIEditor\Src %%a in (*.cpp) do echo     %%a \ >> %OUT_PRO%
 echo. >> %OUT_PRO%
 
 echo HEADERS = \ >> %OUT_PRO%
-for /r %CUR_DIR%\QtBTEditor\Inc %%a in (*.h) do echo     %%a \ >> %OUT_PRO%
+for /r %CUR_DIR%\QtAIEditor\Inc %%a in (*.h) do echo     %%a \ >> %OUT_PRO%
 echo. >> %OUT_PRO%
