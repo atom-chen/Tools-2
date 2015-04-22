ee1539cc4e25c8964a075802c0c853bba149f0b6
aaa <kuangben2001@163.com>
2015-04-22 10:59:26 +0800

    aaa

---
+++ FileDirDiff/FileDirDiff/src/DelPyCache.bat
@@ -0,0 +1,10 @@
+@echo off
+rem echo on
+::cls
+set base_dir=%~dp0
+%base_dir:~0,2%
+cd %base_dir%
+
+::echo %base_dir%
+
+for /r %base_dir% %%i in (__pycache__ pycache _pycache_) do if exist %%i rmdir /s/q "%%i"
