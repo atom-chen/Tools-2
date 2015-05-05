daa5a600157c051583b881dc160dbf348eaa37a0
aaa <kuangben2001@163.com>
2015-05-05 10:43:00 +0800

    aaa

--- FileDirDiff/FileDirDiff/src/cx_Freezer.bat
+++ FileDirDiff/FileDirDiff/src/cx_Freezer.bat
@@ -1,8 +1,15 @@
 set base_dir=%~dp0
 %base_dir:~0,2%
 
-pushd %base_dir%
+cd %base_dir%
 D:\Python34\python.exe cx_Freezer.py build
-popd
+
+rem copy file
+if exist %base_dir%build\exe.win-amd64-3.4\Config rd /s /q %base_dir%build\exe.win-amd64-3.4\Config
+md %base_dir%build\exe.win-amd64-3.4\Config
+xcopy %base_dir%FileDirDiff\Config\*.* %base_dir%build\exe.win-amd64-3.4\Config /s /e /h
+
+rem ren %base_dir%build\exe.win-amd64-3.4\Config\Config_bak.txt %base_dir%build\exe.win-amd64-3.4\Config\Config.txt
+ren %base_dir%build\exe.win-amd64-3.4\Config\Config_bak.txt Config.txt
 
 pause
