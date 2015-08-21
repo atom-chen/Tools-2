9859122cf90fecc64cffb914246c19dfddfd9b53
kuangben <kuangben2010@yeah.net>
2015-08-21 23:57:07 +0800

    aaa

--- ULua/build-win64-Luavm.sh
+++ ULua/build-win64-Luavm.sh
@@ -1,15 +1,9 @@
 cd lua-5.1.5/src
-mingw32-make -fMakefile-Static all
+make -f Makefile-Static all
 cd ../..
+
+make -f makefile-Luavm all
 
 cd luasocket/src
-mingw32-make -fmakefile-Luavm all
+make -f makefile-Luavm all
 cd ../..
-
-gcc lua_wrap.c \
-    -o Plugins/x86_64/ulua.dll -m64 -shared \
-    -I./ \
-    -Ilua-5.1.5/src \
-    -Wl,--whole-archive \
-    window/x86_64/liblua.a \
-    -Wl,--no-whole-archive -lwsock32 -static-libgcc -static-libstdc++
