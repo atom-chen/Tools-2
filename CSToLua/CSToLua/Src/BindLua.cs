8fc6a2b2db52d6cbb564445dff2444125a31112e
codekuangben <kuangben2001@163.com>
2016-02-11 23:54:43 +0800

    aaa

--- CSToLua/CSToLua/Src/BindLua.cs
+++ CSToLua/CSToLua/Src/BindLua.cs
@@ -1,16 +1,8 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System;
-using System.Collections;
-
-using Object = UnityEngine.Object;
+﻿using System;
 using System.Text;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Reflection;
-using UnityEngine.Rendering;
 
 public class BindType {
     public string name;                 //类名称
